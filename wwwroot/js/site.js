868a719049256af5a4de6fde7b6dec9294bba78e
Matt Wagner <m.wagner@dwdiesel.com>
2016-08-31 15:03:43 -0400

    Add JavaScript events

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,12 @@
 ﻿// site.js
 var ele = document.getElementById("username");
 ele.innerHTML = "Matt Wagner";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style = "background-color: #888";
+};
+
+main.onmouseleave = function () {
+    main.style = "";
+};
