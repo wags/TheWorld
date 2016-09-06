c85584b55e99de49b6eab1c9319c1777f0bf9773
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-06 10:10:27 -0400

    Complete the form

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TheWorld.ViewModels;
 using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -31,7 +34,19 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
-            _mailService.SendMail("matt@mail.com", model.Email, "From TheWorld", model.Message);
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("", "We don't support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
 
             return View();
         }
