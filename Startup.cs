39cff1b102f24a54886a1ee1e6ba5a74c92aae7c
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-08 14:52:13 -0400

    Implement logging
    Add support for different levels of logging, depending on the
    environment.

--- Startup.cs
+++ Startup.cs
@@ -51,20 +51,26 @@ namespace TheWorld
 
             services.AddTransient<WorldContextSeedData>();
 
+            services.AddLogging();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
             IHostingEnvironment env, 
-            ILoggerFactory loggerFactory,
+            ILoggerFactory factory,
             WorldContextSeedData seeder)
         {
-            loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
             }
 
             app.UseStaticFiles();
