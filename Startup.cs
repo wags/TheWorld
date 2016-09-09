14ae210c68e5b80e7d1cc2bb130ccecefe9b8c5f
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-09 15:13:20 -0400

    Set up the Stop Controller

--- Startup.cs
+++ Startup.cs
@@ -72,6 +72,7 @@ namespace TheWorld
             Mapper.Initialize(config =>
             {
                 config.CreateMap<TripViewModel, Trip>().ReverseMap();
+                config.CreateMap<StopViewModel, Stop>().ReverseMap();
             });
 
             if (env.IsDevelopment())
