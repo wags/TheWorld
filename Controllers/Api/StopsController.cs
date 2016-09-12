fa8d4410415d31411d6d5b9b2c6518505166912c
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-12 10:36:57 -0400

    Add a method to save new stops

--- Controllers/Api/StopsController.cs
+++ Controllers/Api/StopsController.cs
@@ -39,5 +39,34 @@ namespace TheWorld.Controllers.Api
 
             return BadRequest("Failed to get stops");
         }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post(string tripName, [FromBody]StopViewModel vm)
+        {
+            try
+            {
+                // If the VM is valid
+                if (ModelState.IsValid)
+                {
+                    var newStop = Mapper.Map<Stop>(vm);
+                    // Look up the Geocodes
+
+                    // Save to the Database
+                    _repository.AddStop(tripName, newStop);
+
+                    if (await _repository.SaveChangesAsync())
+                    {
+                        return Created($"/api/trips/{tripName}/stops/{newStop.Name}",
+                            Mapper.Map<StopViewModel>(newStop));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Failed to save new Stop: {0}", ex);
+            }
+
+            return BadRequest("Failed to save new stop");
+        }
     }
 }
