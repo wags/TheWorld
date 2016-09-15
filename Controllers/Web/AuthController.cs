a915c0ec6dbded38db5f8bcbcd16f46b379e5edd
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-15 10:34:28 -0400

    Design the Login View

---
+++ Controllers/Web/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
