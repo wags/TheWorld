ead7ee304388839679e4e051baf203e4b31360a1
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-28 13:25:47 -0400

    Retrieve data using Angular

--- wwwroot/js/tripsController.js
+++ wwwroot/js/tripsController.js
@@ -7,22 +7,31 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        }, {
-            name: "World Trip",
-            created: new Date()
-        }];
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.trips);
+            }, function () {
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
         vm.addTrip = function () {
-            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.trips.push({ name: vm.newTrip.name, dateCreated: new Date() });
             vm.newTrip = {};
         };
 
