538a353811659c4a7e3214a16f2d806e228dece2
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-29 08:18:56 -0400

    Add AngularJS routing

--- wwwroot/js/app-trips.js
+++ wwwroot/js/app-trips.js
@@ -4,6 +4,17 @@
     "use strict";
 
     // Creating the module
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+
+        });
 
 })();
