d2e86bb130b81c1f87f983d4a3f5af889bbe49f7
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-29 10:30:53 -0400

    Call the API to load stops

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -6,13 +6,26 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.stops);
+            }, function () {
+                // Failure
+                vm.errorMessage = "Failed to load stops"
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
     }
 
 })();
