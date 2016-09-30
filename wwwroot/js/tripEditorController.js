beb78ea9eb88288126ba13a9cd559192152549a6
Matt Wagner <m.wagner@dwdiesel.com>
2016-09-30 16:53:21 -0400

    Validate a new stop

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -13,6 +13,7 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
 
         $http.get("/api/trips/" + vm.tripName + "/stops")
             .then(function (response) {
